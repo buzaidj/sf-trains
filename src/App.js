71213f52bf9f392f5fbb1fb3c8954dd00f0f1bef
James Buzaid <jjb422@cornell.edu>
2022-06-03 00:17:13 -0400

    finished filtering logic, part of maps working

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import Select, { InputActionMeta } from 'react-select'
 import opts from './options.json'
 import RefreshIcon from '@mui/icons-material/Refresh';
 
-const options = opts.options
+import 'leaflet/dist/leaflet.css';
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { useMap } from 'react-leaflet/hooks';
+import { Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import { height } from '@mui/system';
+
 
 // const arrivals =
 //   [
@@ -17,7 +25,7 @@ const options = opts.options
 
 
 const key = "80b27b9e-f65e-4c32-960a-a40a076561ba"
-const REFRESH_TIME = 1200000; // 20 mins  
+const REFRESH_TIME = 120000; // 2 mins  
 const MAX_ARRIVALS = 7;
 
 function toTitleCase(str) {
@@ -41,11 +49,11 @@ function Arrivals(props) {
 function Mins(props) {
   switch (props.mins) {
     case 0:
-      return <p className='mins'>Arriving <span className='minsNum'>now</span></p>;
+      return <p className='mins'>Arriving <span className='arrivingNow'>now</span></p>;
     case 1:
       return <p className='mins'><span className='minsNum'>{props.mins}</span> min away</p>;
     default:
-      return <p className='mins'><span className='minsNum'>{props.mins}</span> min away</p>;
+      return <p className='mins'><span className='minsNum'>{props.mins}</span> mins away</p>;
   }
 }
 
@@ -55,8 +63,16 @@ function longDirection(dir) {
       return 'Inbound';
     case 'OB':
       return 'Outbound';
+    case 'W':
+      return 'West';
+    case 'E':
+      return 'East';
+    case 'N':
+      return 'North';
+    case 'S':
+      return 'South';
     default:
-      return dir
+      return dir;
   }
 }
 
@@ -72,8 +88,20 @@ function ArrivalRow(props) {
   </div>;
 }
 
-function computeArrivalMins(expectedArrivalTime) {
-  return Math.floor(Math.abs((Date.now() - Date.parse(expectedArrivalTime)) / 1000 / 60));
+function computeMins(expectedArrivalTime) {
+  console.log(Date.parse(expectedArrivalTime) - Date.now());
+  console.log(Date.parse(expectedArrivalTime));
+  console.log(expectedArrivalTime);
+  return Math.floor(Math.abs((Date.parse(expectedArrivalTime) - Date.now()) / 1000 / 60));
+}
+
+function getExpectedTime(j) {
+  for (const time of [j.MonitoredCall.ExpectedDepartureTime, j.MonitoredCall.ExpectedArrivalTime]) {
+    if (time) {
+      return time;
+    }
+  }
+  return null;
 }
 
 function createArrivals(data) {
@@ -84,7 +112,16 @@ function createArrivals(data) {
       lineName: j.PublishedLineName,
       direction: j.DirectionRef,
       destination: j.DestinationName,
-      mins: computeArrivalMins(j.MonitoredCall.ExpectedArrivalTime)
+      mins: computeMins(getExpectedTime(j))
+    }))
+}
+
+function createStops(data) {
+  return data?.Contents.dataObjects.ScheduledStopPoint
+    .map(x => Object({
+      value: x.id,
+      label: x.Name,
+      location: [x.Location.Latitude, x.Location.Longitude]
     }))
 }
 
@@ -93,11 +130,24 @@ function toTimeStr(date) { return new Date(date).toLocaleTimeString([], { hour: 
 
 
 function App() {
-  const [stop, setStop] = useState(options[0])
-  const [lastUpdateTime, setUpdateTime] = useState(Date.now())
-  const [arrivalList, setArrivals] = useState([])
+  const [stop, setStop] = useState({
+    value: '16995',
+    label: 'Powell Station Outbound'
+  });
+  const [lastUpdateTime, setUpdateTime] = useState(Date.now());
+  const [arrivalList, setArrivals] = useState([]);
+  const [filteredLines, setFilteredLines] = useState([]);
+  const [stops, setStops] = useState([]);
+
+  function FetchStops() {
+    fetch(`http://api.511.org/transit/stopplaces?api_key=${key}&operator_id=SF&format=json`)
+      .then(res => res.json())
+      .then(res => { setStops(createStops(res)) })
+      .catch(error => console.log(error.message));
+  }
 
   function FetchData(stop) {
+    console.log("Fetching data!");
     fetch("http://api.511.org/transit/StopMonitoring?api_key=" + key + "&agency=SF&format=json&stopcode=" + stop.value)
       .then(res => res.json())
       .then(res => { setArrivals(createArrivals(res)) })
@@ -105,9 +155,12 @@ function App() {
   }
 
   useEffect(() => {
+    setUpdateTime(Date.now());
     FetchData(stop);
+    FetchStops();
 
     const interval = setInterval(() => {
+      setUpdateTime(Date.now());
       FetchData(stop);
     }, REFRESH_TIME);
 
@@ -122,13 +175,12 @@ function App() {
         <span className='bold'> {props.station}</span>.
         Last updated at {toTimeStr(props.lastUpdateTime)}.
       </p>
-      <button className='refresh' onClick={() => { setUpdateTime(Date.now()) }}><RefreshIcon></RefreshIcon></button>
+      <button className='refresh' onClick={() => { setUpdateTime(Date.now()); FetchData(stop); }}><RefreshIcon></RefreshIcon></button>
     </div>
   }
 
 
   function onChange(valueMeta, actionMeta) {
-    console.log(actionMeta.action);
     switch (actionMeta.action) {
       case 'select-option':
       case 'set-value':
@@ -143,8 +195,62 @@ function App() {
 
 
   const StopSelector = () => (
-    <Select id="StopSelector" options={options} onChange={onChange} />
+    <Select id="StopSelector" options={stops} onChange={onChange} />
   );
+
+  function filterIconStyle(lineCode) {
+    if (!filteredLines.includes(lineCode)) {
+      return iconStyle(lineCode);
+    }
+    else {
+      return { 'backgroundColor': 'gray', 'color': 'lightgray' };
+    }
+  }
+
+  function filterLineClick(lineCode) {
+    if (!filteredLines.includes(lineCode)) {
+      setFilteredLines(filteredLines.concat([lineCode]));
+    }
+    else {
+      setFilteredLines(filteredLines.filter((v, _) => v !== lineCode));
+    }
+  }
+
+  function Filtering() {
+    var lineCodes = [...new Set(arrivalList.map(x => x.lineCode))];
+    return <div className='Filtering'>
+      <p>Filter:</p>
+      {
+        lineCodes.map((x) => {
+          return <i style={filterIconStyle(x)} className='lineIconFilter' onClick={() => filterLineClick(x)}>{x}</i>;
+        }
+        )
+      }
+    </div >
+  }
+
+  function MarkerFromStop(props) {
+    return <Marker position={props.stop.location} icon={new Icon({ iconUrl: markerIconPng })}>
+      <Popup>
+        {props.stop.label} <br />
+        <button onClick={
+          () => {
+            setUpdateTime(Date.now());
+            setStop(props.stop);
+            FetchData(props.stop);
+          }
+        }>Select this stop</button>
+      </Popup>
+    </Marker>
+  }
+
+  function MarkersFromStops(props) {
+    var markers = [];
+    for (var stop of props.stops) {
+      markers.push(<MarkerFromStop stop={stop}></MarkerFromStop>);
+    }
+    return <div>{markers}</div>
+  }
 
 
   return (
@@ -153,7 +259,18 @@ function App() {
         <HeaderRow station={stop.label} lastUpdateTime={lastUpdateTime}></HeaderRow>
 
         <StopSelector></StopSelector>
-        <Arrivals liProps={arrivalList.slice(0, MAX_ARRIVALS)}></Arrivals>
+        {/* <MapContainer center={[37.7749, -122.4194]} zoom={14} scrollWheelZoom={false} style={{ height: '500px' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <div>
+            <MarkersFromStops stops={stops}></MarkersFromStops>
+          </div>
+
+        </MapContainer> */}
+        <Filtering></Filtering>
+        <Arrivals liProps={arrivalList.filter(x => !filteredLines.includes(x.lineCode)).slice(0, MAX_ARRIVALS)}></Arrivals>
 
       </header>
     </div >
--- src/arrivalHelpers.js
+++ src/arrivalHelpers.js
@@ -3,6 +3,11 @@ import './App.css';
 /** styling icons  */
 export function iconStyle(lineCode) {
     switch (lineCode) {
+        case 'C':
+            return {
+                'background-color': 'rgb(180, 154, 54)',
+                'color': 'black'
+            }
         case 'KT':
             return {
                 'background-color': 'black',
@@ -13,6 +18,12 @@ export function iconStyle(lineCode) {
             return {
                 'background-color': 'rgb(0, 133, 71)',
                 'color': 'rgb(255, 255, 255)'
+            }
+        case 'J':
+            return {
+                'background-color': '#a96614',
+                'color': '#fff'
+
             }
         case 'S':
             return {
